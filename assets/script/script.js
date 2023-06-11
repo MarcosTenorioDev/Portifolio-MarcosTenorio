049345f6960db2d6cf24bead2d8add447072f760
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-06-10 23:58:49 -0300

    :iphone: add responsive design to navbar in tablet size

--- assets/script/script.js
+++ assets/script/script.js
@@ -63,3 +63,8 @@ observer.observe(card1, observerOptions);
 observer.observe(card2, observerOptions);
 observer.observe(card3, observerOptions);
 
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
